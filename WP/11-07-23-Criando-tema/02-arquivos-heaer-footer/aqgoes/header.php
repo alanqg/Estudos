7a43638de87db901a03dc108b3a44ff55e4fef00
Alan Queirós de Goes <alanqg@gmail.com>
2023-08-02 09:34:23 -0300

    Partes dos arquivos do tema
    Foi feito a divisão do tema com os arquivos header é footer.

---
+++ WP/11-07-23-Criando-tema/02-arquivos-heaer-footer/aqgoes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AqGoes</title>
+</head>
+<body <?php body_class(); ?>> <!---Chamado para mudar algumas classes em casa página, isso facilita na hora de fazer estilos diferentes para páginas diferentes -->
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut facere odit, dolor qui dolore fugit eius! Porro molestiae quae architecto repellendus deserunt omnis, ab sint praesentium quod nobis necessitatibus blanditiis?</p>
+
+
+    <header>
+        <section class="top-bar">
+
+        <!--Área de pesquisa é media social-->    
+        <div class="social-media-icons"></div>
+            <div class="search"></div>
+
+            <!--Area de menus-->
+            <section class="menu-area">
+                <section class="logo"></section>
+                <nav class="menu"></nav>
+            </section>
+         
+
+        </section>
+    </header>
---
+++ WP/11-07-23-Criando-tema/02-arquivos-heaer-footer/aqgoes/index.php
@@ -0,0 +1,18 @@
+
+<?php get_header(); ?>
+    <!--Área do contúdo-->
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <!--Conteudo lateral-->
+            <section class="middle-area">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+            </section>
+
+            <!--Área de como chegar mapa localização-->
+            <section class="map"></section>
+        </main>
+    </div>
+<?php get_footer(); ?>
