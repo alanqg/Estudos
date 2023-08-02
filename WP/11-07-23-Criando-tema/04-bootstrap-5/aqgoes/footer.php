e700da416baf8ef0aa3f7309c1228ac1aa9f614f
Alan Queirós de Goes <alanqg@gmail.com>
2023-08-02 11:01:36 -0300

    Bootstrap
    Foi adicionado a folha de estilo do bootstrap é o js na versão 5.4.3 do bootstrap.

---
+++ WP/11-07-23-Criando-tema/04-bootstrap-5/aqgoes/footer.php
@@ -0,0 +1,10 @@
+
+
+
+<footer>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti a est minus libero mollitia optio, eos natus suscipit. Nihil voluptate eaque, iusto quod possimus dolorum aut eum non nam sapiente.</p>
+</footer>
+<!--Umas das principais funções é para chamar os scripts no footer do site-->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ WP/11-07-23-Criando-tema/04-bootstrap-5/aqgoes/functions.php
@@ -0,0 +1,16 @@
+<?php
+/*Carregar os scripts*/
+function load_scripts(){
+
+    /*Chamando o js do bootstrap*/
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js', array(  ), '5.3.1', true );
+
+    /*Folha de estilo do bootstrap*/
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css', array( ), '5.3.1', 'all' );
+    /*Primeoro paramentro o nome do arquivo, segundo parametro é o caminho, outro parametro é para ver se existem depecias do aquivo no caso o css,depois a versão é qual tipo e média no casal aqui todas */
+    wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
+}
+
+/*Associando o nome da função ao gancho WP, conforme a foto do arquivo referente aos ganchos WP*/
+add_action( 'wp_enqueue_scripts', 'load_scripts' );
+?>
