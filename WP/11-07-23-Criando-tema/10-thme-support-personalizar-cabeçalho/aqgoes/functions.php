eb0763ea9fdd37245ba6ad2ab091a0c5478b2619
Alan Queirós de Goes <alanqg@gmail.com>
2023-08-03 12:49:22 -0300

    theme support

---
+++ WP/11-07-23-Criando-tema/10-thme-support-personalizar-cabeçalho/aqgoes/functions.php
@@ -0,0 +1,44 @@
+<?php
+/*Carregar os scripts*/
+function load_scripts(){
+
+    /*Chamando o js do bootstrap*/
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js', array(  ), '5.3.1', true );
+
+    /*Folha de estilo do bootstrap*/
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css', array( ), '5.3.1', 'all' );
+    /*Primeoro paramentro o nome do arquivo, segundo parametro é o caminho, outro parametro é para ver se existem depecias do aquivo no caso o css,depois a versão é qual tipo e média no casal aqui todas */
+    wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
+}
+/*Associando o nome da função ao gancho WP, conforme a foto do arquivo referente aos ganchos WP*/
+add_action( 'wp_enqueue_scripts', 'load_scripts' );
+
+
+/*Função de confiração do tema*/
+function aqgoes_config(){
+    /*Área para registro de menus*/
+    register_nav_menus(
+        array(
+            'my_main_menu' => 'Main Menu',
+            'my_second_menu' => 'second Menu'
+
+    )
+);
+
+
+/*theme_suporte*/ 
+
+/*Criando ao altura é a largura do cabecalho*/
+$args = array(
+    'height' => 225,
+    'width' => 1920
+);
+/*Suporte ao cabeçalho, primeiro argumento para customizar o cabeçalho, segundo argumento foi o do array criado acima para definir o tamanho é a largura*/
+ add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'aqgoes_config', 0 );
+
+
+
+
+?>
---
+++ WP/11-07-23-Criando-tema/10-thme-support-personalizar-cabeçalho/aqgoes/index.php
@@ -0,0 +1,73 @@
+
+<?php get_header(); ?>
+
+<!--chamada do personalização do cabecalho-->
+<img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="">
+<!--Área do contúdo-->
+    <div class="content-area">
+        <main>
+            <section class="slide">
+                <div class="container">
+                    <div class="row">
+                        Slide
+                    </div>
+                </div>
+            </section><!--Finalizando o slide-->
+            
+            <section class="services">
+                <div class="container">
+                    <div class="row">
+                        Serviços
+                    </div>
+                </div>
+            </section><!--Finalizando serviços-->
+
+            <!--Conteudo lateral-->
+            <section class="middle-area">
+                <div class="container">
+                    <div class="row">
+                      <aside class="sidebar col-md-4">BArra Lateral</aside>
+                      <div class="news col-md-8">
+                        <?php
+                        //Tive posts 
+                            if( have_posts() ): 
+                                //Enquanto tiver posts, mostrar par mim
+                                while( have_posts() ): the_post();
+                         ?>
+                               <!--Aqui será mostrado os posts, essas chamado são com comando para chamar titulos tags, categoria etc, essas tags são chamadas de tampletes tags-->
+
+                                    <article>
+                                        <h2><?php the_title(); ?></h2>
+                                        <p>Published in <?php echo get_the_date(); ?>  by <?php the_author_posts_link(); ?> </p>
+                                        <!-- o argumento usado no the_caegory ( ' ' ) para dar espaço quando tiver mais de uma categoria-->
+                                        <p>Categories: <?php the_category( ' ' ); ?></p>
+                                        <!--Primeiro argumento será um o nome tag com os dois pontos, já o segundo argumento é a virgula é para separa uma tag da outra -->
+                                        <p><?php the_tags('Tags:', ','); ?></p>
+
+                                        <!--Função chamará o conteúdo do post-->
+                                        <?php the_content(); ?>
+
+                                    </article>
+                         <?php
+                            endwhile;
+                         else:
+                         ?>
+
+                         <p>Não tem post disponível....</p>
+
+                         <?php endif; ?>
+
+                      </div>
+                    </div>
+                </div>
+            </section><!--Finalizando barra lateral é noticias-->
+
+            <!--Área de como chegar mapa localização-->
+            <section class="map">
+                <div class="container">
+                    <div class="row">Mapa</div>
+                </div>
+            </section>
+        </main>
+    </div>
+<?php get_footer(); ?>
