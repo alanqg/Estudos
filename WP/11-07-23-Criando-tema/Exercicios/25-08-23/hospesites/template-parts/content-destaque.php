a6f3b0baa4510d71e7464c489e4dc7264890553e
Alan Queirós de Goes <alanqg@gmail.com>
2023-08-31 13:37:18 -0300

    Modelo Exercícios

--- WP/11-07-23-Criando-tema/Exercicios/25-08-23/hospesites/template-parts/content-destaque.php
+++ WP/11-07-23-Criando-tema/Exercicios/25-08-23/hospesites/template-parts/content-destaque.php
@@ -1,2 +1,17 @@
-<?php the_title(); ?>
-<?php the_content(); ?>
+<article <?php post_class(array( 'class' => 'destaque')); ?>>
+    <div class="thumbnail">
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large', array('class' => 'img-fluid' )); ?></a>
+    </div>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+    <!--Copor dos post em destaque -->
+    <div class="meta_info">
+        <p>
+            Criado por: <span><?php the_author_posts_link(); ?></span>
+            Categoria: <span><?php the_category(' ');?></span>
+            <?php the_tags('Tags: <span>', ',', '</span>'); ?>
+        </p>
+        <p><span><?php echo get_the_date(); ?></span></p>
+    </div>
+    <?php the_excerpt(); ?>
+</article>
