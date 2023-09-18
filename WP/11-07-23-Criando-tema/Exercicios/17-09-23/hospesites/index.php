b0b1df53022ff01fc1314e44dd4e67864d24d40f
Alan Queirós de Goes <alanqg@gmail.com>
2023-09-18 08:37:22 -0300

    exercício pagina home|blog

--- WP/11-07-23-Criando-tema/Exercicios/17-09-23/hospesites/index.php
+++ WP/11-07-23-Criando-tema/Exercicios/17-09-23/hospesites/index.php
@@ -1,5 +1,37 @@
+
+
 <?php get_header(); ?>
-    <main>
-       <h1>Página Posts</h1>
-    </main>
-    <?php get_footer(); ?>
+<main>  
+    <section class="posts">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-4">
+                  <aside>
+                     <h2>Sibebar</h2>
+                  </aside>
+                   </div>
+                <div class="col-md-8">
+                    <article>
+                    <h1>Página Posts</h1>
+                        <?php
+                           if( have_posts() ) : 
+                              while( have_posts() ) : the_post();
+                        ?>
+
+                           <?php get_template_part('template-parts/content', get_post_format() ); ?>
+
+                              <?php 
+                                 endwhile;
+                              else:
+                              ?>
+
+                           <p>Mão tem nada para mostrar</p>
+                           <?php endif; ?>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+<?php get_footer(); ?>
