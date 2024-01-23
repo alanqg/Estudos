f1d3da1fa908da9414576731e7ec19e8db646a1e
Alan Queirós de Goes <alanqg@gmail.com>
2024-01-23 08:46:39 -0300

    Edição Exercícios

--- HTMLCSS/exercicios/03-eercios-dias/js/customer.js
+++ HTMLCSS/exercicios/03-eercios-dias/js/customer.js
@@ -4,5 +4,7 @@
 function muda_nomeN(){
     alert('Aqgoe');
 }
-let x = 2;
-document.write(x + " Aqgoes") 
+
+function escreve_na_tela(){
+    document.getElementById("escreve_na_tela").innerHTML =  "aqgoes";
+}
